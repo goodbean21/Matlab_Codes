24c562880deb0d7ea1873dd825d93ed4708a68e8
jayk2 <abelca.a19@gmail.com>
2019-05-14 21:39:41 -0500

    calculo de resistencias
    Funciones para calcular el valor real de resistencia de nuestro sistema

--- calcularResistencias.m
+++ calcularResistencias.m
@@ -1,4 +1,18 @@
 function calcularResistencias(angle1, angle2)
+    %   Se reciben dos valores de ángulo en grados donde el primer
+    %   parametro es la resistencia aórtica y el segundo parametro es
+    %   referente a la resistencia sistémica. Resultante de la
+    %   caracterización. La función despliega el valor de ambas
+    %   resistencias dado dos ángulos.
+    %
+    %   :param angle1: Ángulo de la perilla que simula la resistencia
+    %   aórtica
+    %       :angle double: Es un valor double del Ángulo a evaluar
+    %   
+    %   :param angle2: Ángulo de la perilla que simula la resistencia
+    %   sistémica
+    %       :angle double: Es un valor double del Ángulo a evaluar
+    
     res(1) = valor_Resistencia1(angle1);
     res(2) = valor_Resistencia2(angle2);
     
